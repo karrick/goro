8123f3c62f7c7639535ef85e5d6cca82c04d1a0c
Karrick S. McDermott <karrick@karrick.net>
2018-09-07 10:27:39 -0400

    Once is now uint32 rather than a structure

--- goro.go
+++ goro.go
@@ -6,13 +6,11 @@ import (
 
 // Once is an object that will perform an action exactly one time, until reset,
 // at which point, it will only perform an action once.
-type Once struct {
-	done uint32
-}
+type Once uint32
 
 // Do invokes f exactly one time, regardless of how many times Do is called.
 func (o *Once) Do(f func()) {
-	if atomic.CompareAndSwapUint32(&o.done, 0, 1) {
+	if atomic.CompareAndSwapUint32((*uint32)(o), 0, 1) {
 		f()
 	}
 }
@@ -20,5 +18,5 @@ func (o *Once) Do(f func()) {
 // Reset will allow the next invocation of Do to perform the specified action
 // once again.
 func (o *Once) Reset() {
-	atomic.StoreUint32(&o.done, 0)
+	atomic.StoreUint32((*uint32)(o), 0)
 }
